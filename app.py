d1607f9d9a764959e2e0e76c219988c5785248ce
ajaysartaj-gif <ajaysartaj@gmail.com>
2026-05-07 21:39:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -570,16 +570,16 @@ if st.button("Analyze"):
         # ADVANCED RISK
         # -------------------------------
 
-    try:
-           advanced_risk_reasoning(
-              semantic_objects
-              )
+      try:
+            advanced_risk_reasoning(
+               semantic_objects
+               )
 
-    except Exception as e:
+     except Exception as e:
 
             advanced_risk = {
                 "error": str(e)
-            }
+             }
         # -------------------------------
         # CHANGES DETECTED
         # -------------------------------
