646a7efe821d2ba96165c438c3758c3dd066b07e
ajaysartaj-gif <ajaysartaj@gmail.com>
2026-05-07 09:53:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import os
 import streamlit as st
 from openai import OpenAI
+from taxonomy import SEMANTIC_TAXONOMY
 
 HISTORY_FILE = "change_history.json"
 # -------------------------------
